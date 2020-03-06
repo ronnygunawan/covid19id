8c1cce8f7fef588d0e4bfacc445668be0b3de369
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2020-03-06 15:16:10 +0700

    Setup webpack

---
+++ src/webpack.config.js
@@ -0,0 +1,36 @@
+"use strict";
+var path = require("path");
+
+module.exports = {
+	mode: "production",
+	entry: {
+        app: path.resolve("./ts/index.tsx")
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve("./js/")
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".jsx"]
+	},
+	module: {
+		rules: [
+			{
+                test: /\.(t|j)s(x?)$/,
+				exclude: /node_modules/,
+                use: [
+					{
+						loader: 'ts-loader'
+					}
+				]
+			},
+			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+		]
+	},
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM",
+		"jquery": "jQuery"
+	},
+	devtool: "source-map"
+};
