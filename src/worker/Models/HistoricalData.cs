26ece327ca6aa7e04ffd3b09a640d928faede1fc
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2020-03-18 06:28:56 +0700

    WIP JHUCSSE client

---
+++ src/worker/Models/HistoricalData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+
+namespace Covid19id.Models {
+	public class HistoricalData {
+		public string Country { get; }
+		public string? Province { get; }
+		public double Latitude { get; }
+		public double Longitude { get; }
+		public ImmutableList<HistoricalDatum> TimeSeries { get; }
+
+		public HistoricalData(
+			string country,
+			string? province,
+			double latitude,
+			double longitude,
+			ImmutableList<HistoricalDatum> timeSeries
+		) {
+			Country = country;
+			Province = province;
+			Latitude = latitude;
+			Longitude = longitude;
+			TimeSeries = timeSeries;
+		}
+	}
+}
---
+++ src/worker/Models/ProvinceSummary.cs
@@ -0,0 +1,20 @@
+﻿namespace Covid19id.Models {
+	public class ProvinceSummary {
+		public string Name { get; }
+		public int Confirmed { get; }
+		public int Deceased { get; }
+		public int Recovered { get; }
+
+		public ProvinceSummary(
+			string name,
+			int confirmed,
+			int deceased,
+			int recovered
+		) {
+			Name = name;
+			Confirmed = confirmed;
+			Deceased = deceased;
+			Recovered = recovered;
+		}
+	}
+}
